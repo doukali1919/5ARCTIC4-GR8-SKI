68a634bb959834529540bc8f20c20ce86d7f2c2b
doukali1919 <youcef.doukali@gmail.com>
2024-11-05 03:37:27 +0100

    first commit

---
+++ course-management/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import CourseManager from './components/CourseManager';
+
+const App = () => {
+    return (
+        <div className="App">
+            <CourseManager />
+        </div>
+    );
+};
+
+export default App;
